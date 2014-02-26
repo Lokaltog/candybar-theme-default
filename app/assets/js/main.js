598937239f02fe2a124040e01207312e3e87d9d9
Kim Silkebækken <kim.silkebaekken@gmail.com>
2014-02-26 18:55:49 +0100

    Move datetime updater to wkline controller

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -2,7 +2,8 @@
 // available for data injection into the controller scope
 var wkInject,
     wkHandlers,
-    merge_recursive
+    merge_recursive,
+    pad
 
 merge_recursive = function (obj1, obj2) {
 	// http://stackoverflow.com/questions/171251/how-can-i-merge-properties-of-two-javascript-objects-dynamically
@@ -24,6 +25,12 @@ merge_recursive = function (obj1, obj2) {
 	return obj1
 }
 
+pad = function (n, width, z) {
+	z = z || '0'
+	n = n + ''
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n
+}
+
 wkHandlers = {
 	widget: function ($scope, data) {
 		if (! $scope.widget.hasOwnProperty(data._widget)) {
@@ -35,7 +42,23 @@ wkHandlers = {
 
 angular.module('Wkline', [])
 	.controller('WklineCtrl', ['$scope', function ($scope) {
-		$scope.widget = {}
+		$scope.widget = {};
+
+		var updateDatetimeWidget = function () {
+			var now = new Date()
+			$scope.widget.datetime = {
+				date: now.getFullYear() + '-' +
+					pad(now.getMonth() + 1, 2) + '-' +
+					pad(now.getDate(), 2),
+				time: pad(now.getHours(), 2) + ':' +
+					pad(now.getMinutes(), 2),
+			}
+		}
+
+		// For some reason $scope.$apply breaks if run outside this setTimeout block
+		setTimeout(function () { $scope.$apply(updateDatetimeWidget)}, 1)
+		setInterval(function () { $scope.$apply(updateDatetimeWidget)}, 10000)
+
 		wkInject = function (payload) {
 			if (! payload) {
 				return false
